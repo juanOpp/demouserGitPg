7d7ab4b851be84c9ca6c522c1b3e673c77414fd1
juanOpp <sandra.lozanosanchez@plexus.local>
2022-09-21 15:54:44 +0200

    cors

---
+++ src/main/java/com/opplus/demouserGitPG/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.opplus.demouserGitPG.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.opplus.demouserGitPG.filter.CORSFilter;
+
+
+
+
+
+@Configuration
+public class WebConfig {
+
+    @Autowired
+    private CORSFilter corsFilter;
+
+    @Bean
+    public FilterRegistrationBean<CORSFilter> corsFilter() {
+        FilterRegistrationBean<CORSFilter> registration = new FilterRegistrationBean<CORSFilter>();
+        registration.setFilter(corsFilter);
+        registration.addUrlPatterns("/*");
+        registration.setName("corsFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+}
